0c250f207f4d58ac4ff8e17a315fc7cd8efbddda
recep ahmet kara <recepahmetkara@gmail.com>
2023-06-21 23:10:52 +0300

    Update id-generator.go

--- API/internal/generator/id-generator.go
+++ API/internal/generator/id-generator.go
@@ -10,16 +10,13 @@ func NewIDGenerator() (string, error) {
 }
 
 func generateRandomID() (string, error) {
-	// ID'nin byte dizisi için bir tampon oluştur
 	id := make([]byte, 16)
 
-	// Rastgele verilerle tamponu doldur
 	_, err := rand.Read(id)
 	if err != nil {
 		return "", err
 	}
 
-	// Base64 ile kodlayarak rastgele veriyi dizeye dönüştür
 	encodedID := base64.URLEncoding.EncodeToString(id)
 
 	return encodedID, nil
